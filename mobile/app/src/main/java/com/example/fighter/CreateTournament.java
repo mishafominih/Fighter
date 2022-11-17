88e92bc60f54c52055858091359cf64cf109b73f
mishafominih <53509997+mishafominih@users.noreply.github.com>
2022-11-17 17:25:59 +0500

    Прописал запрос на создание соревнования и убрал синий экран сверху

--- mobile/app/src/main/java/com/example/fighter/CreateTournament.java
+++ mobile/app/src/main/java/com/example/fighter/CreateTournament.java
@@ -4,6 +4,7 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -15,6 +16,10 @@ import com.example.fighter.list_view_helpers.Distribution;
 import com.example.fighter.list_view_helpers.DistributionAdapter;
 import com.example.fighter.list_view_helpers.Range;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 public class CreateTournament extends AppCompatActivity {
@@ -24,9 +29,11 @@ public class CreateTournament extends AppCompatActivity {
     ActivityResultLauncher<Intent> mStartForResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
             result -> {
                 Intent data = result.getData();
-                ArrayList<String> min = data.getStringArrayListExtra("min");
-                ArrayList<String> max = data.getStringArrayListExtra("max");
-                adapter.update_item(min, max, last_pos);
+                if(data != null){
+                    ArrayList<String> min = data.getStringArrayListExtra("min");
+                    ArrayList<String> max = data.getStringArrayListExtra("max");
+                    adapter.update_item(min, max, last_pos);
+                }
             });
 
     @Override
@@ -43,7 +50,43 @@ public class CreateTournament extends AppCompatActivity {
         adapter = new DistributionAdapter(this, R.layout.distribution_list_item, distributions, this);
         fights_list.setAdapter(adapter);
 
+        findViewById(R.id.add_new_distribution).setOnClickListener((view) -> {
+            adapter.add(new Distribution("", ""));
+        });
 
+        findViewById(R.id.button_create_tournament).setOnClickListener((view) -> {
+            MyRequest request = new MyRequest();
+            request.put("name", findViewById(R.id.name));
+            request.put("description", findViewById(R.id.description));
+            request.put("categories", create_categories());
+//            request.Call("create_tournament", (res) -> {
+//
+//            });
+        });
+    }
+
+    @NonNull
+    private JSONArray create_categories() {
+        JSONArray categories = new JSONArray();
+        for(int i = 0; i < adapter.getCount(); i++){
+            Distribution distribution = adapter.getItem(i);
+            JSONObject item = new JSONObject();
+            try {
+                item.put("name", distribution.Name);
+                JSONArray ranges = new JSONArray();
+                for(Range r : distribution.Ranges){
+                    JSONObject range = new JSONObject();
+                    range.put("start", r.Min);
+                    range.put("end", r.Max);
+                    ranges.put(range);
+                }
+                item.put("range", ranges);
+                categories.put(item);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return categories;
     }
 
     public void change_distribution(int position, Intent intent){
